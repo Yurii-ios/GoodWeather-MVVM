57031ea3eac2d6022696cbc7a3b66a60c0acf86a
Yurii <Yurii>
2021-01-27 15:07:07 +0100

    create WeatherListTableViewController and  AddWeatherCityViewController

---
+++ GoodWeather-MVVM/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather-MVVM
+//
+//  Created by Yurii Sameliuk on 27/01/2021.
+//
+
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather-MVVM/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather-MVVM
+//
+//  Created by Yurii Sameliuk on 27/01/2021.
+//
+
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
