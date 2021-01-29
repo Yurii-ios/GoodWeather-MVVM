522ded7c9c1924acefa79829205255439e94ee0d
Yurii <Yurii>
2021-01-29 19:12:34 +0100

    create class SettingsTableViewController

---
+++ GoodWeather-MVVM/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather-MVVM
+//
+//  Created by Yurii Sameliuk on 29/01/2021.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
